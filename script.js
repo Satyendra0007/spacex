4e7c0fb7f73a467f96ca2d59d266170255aa5396
satyendra <skchandrawansi03@gmail.com>
2023-03-11 08:19:07 +0530

    iniial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("hamburger").onclick = ()=>{
+    let menu = document.getElementById("menu")
+   if(menu.style.display == "block")
+        menu.style.display = 'none'
+    else
+        menu.style.display = 'block'
+}
